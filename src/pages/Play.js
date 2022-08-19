3a80001ddec4fe93b8a3e93b85f3797a9eea2875
tszhoip <tszhoip@gmail.com>
2022-08-18 21:18:29 -0700

    clean up

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,48 +1,44 @@
 import React from 'react';
-import Container0 from '../style/container0';
-import BlockImg4 from '../component/BlockImg4';
+import BlockImgCon2 from '../component/BlockImgCon2';
+import BlockInfo from '../component/BlockInfo';
+import lo1 from '../assets/tokyo/tokyo3-01.jpg';
+import lo2 from '../assets/tokyo/tokyo3-02.jpg';
+import lo3 from '../assets/tokyo/tokyo3-03.jpg';
+import lo4 from '../assets/tokyo/tokyo3-04.jpg';
+import lo5 from '../assets/tokyo/tokyo3-05.jpg';
+import lo6 from '../assets/tokyo/tokyo3-06.jpg';
+import lo7 from '../assets/tokyo/tokyo3-07.jpg';
+import lo8 from '../assets/tokyo/tokyo3-08.jpg';
+import lo9 from '../assets/tokyo/tokyo3-09.jpg';
+import lo10 from '../assets/tokyo/tokyo3-10.jpg';
+import lo11 from '../assets/tokyo/tokyo3-11.jpg';
+import lo12 from '../assets/tokyo/tokyo3-13.jpg';
+import lo13 from '../assets/tokyo/tokyo3-14.jpg';
 
 
 
-import 'photoswipe/dist/photoswipe.css'
-import 'photoswipe/dist/default-skin/default-skin.css'
+const ImgLogo = [
+  lo1,
+  lo2,
+  lo3,
+  lo4,
+  lo5,
+  lo6,
+  lo7,
+  lo8,
+  lo9,
+  lo10,
+  lo11,
+  lo12,
+  lo13
+]
 
 function Play() {
-  const Play2021 = [
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-  ]
-
-  const Play2020 = [
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-    'https://source.unsplash.com/weekly?sky&w=1024&h=768',
-  ]
-
-
   return (
-    <Container0 bgcolor="white">
-
-      <BlockImg4
-        images={Play2021} />
-
-      <BlockImg4
-        images={Play2021} />
-
-
-
-    </Container0>
+    <div>
+      <BlockInfo text1="play" text2="try" bgc="white"/>
+      <BlockImgCon2 images={ImgLogo} />
+    </div>
   );
 }
 
