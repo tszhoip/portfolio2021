86a15dc73a2838b585224aab521a2ace1d6a788c
tszhoip <tszhoip@gmail.com>
2022-08-22 16:40:00 -0700

    clean up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -65,7 +65,7 @@ function Home() {
         <Thumbnail img={logo} to="logo" />
         <Thumbnail img={figs} />
       </Worklist>
-      <Col2>
+      {/* <Col2>
         <div lang="zh" >
           <Type2
             b="0.5px solid #f5f5f5"
@@ -81,7 +81,16 @@ function Home() {
             pb="32px"
           />
         </div>
-      </Col2>
+      </Col2> */}
+      <BlockInfo
+        text1="我们是一对设计师夫妇，居住在洛杉矶。相信设计是解决问题的过程。态度是我们设计的基本要素。就像我们不需要一个实体来定义我们一样，我们的工作就是如此。当我们不工作时，我们喜欢喝果味咖啡和享受南亚美食。"
+        text2="We are duo designer couple resides in Los Angeles. Believing design is a process of solving problem. Attitude is essential element of our design. Just like we don’t need an entity to define us, Our work does.
+    Whenever we are not working, we love drinking fruity coffee and enjoying southern Asian food. "
+        pb="32px"
+        b1="0.5px solid #f5f5f5"
+        b2="0.5px solid #f5f5f5"
+      />
+      
     </div>
   );
 }
