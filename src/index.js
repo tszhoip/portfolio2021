1126b077ca1dd10957d6a7e2412bdb4c529364e5
tsz <tszhoip@gmail.com>
2026-08-25 18:54:48 -0700

    fix: remove unnecessary font imports and fix CSS font paths
    - Removed font imports from index.js (fonts are loaded via CSS)
    - Fixed font paths in app.css from '../Assets/font/' to '../assets/font/'
    - Fonts are properly handled by @font-face in CSS

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { base } from './theme';
-import './assets/font/AirPro.ttf';
-import './assets/font/NewFont-Regular.ttf';
-import './assets/font/N1MONO-MONO.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
