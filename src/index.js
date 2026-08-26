8a1d33648d45dc59b8eba5bf688222d1675efa56
tsz <tszhoip@gmail.com>
2026-08-25 18:53:23 -0700

    fix: correct font import paths in index.js
    Changed from '../src/assets/' to './assets/' since index.js is already in src/

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { base } from './theme';
-import '../src/assets/font/AirPro.ttf';
-import '../src/assets/font/NewFont-Regular.ttf';
-import '../src/assets/font/N1MONO-MONO.ttf';
+import './assets/font/AirPro.ttf';
+import './assets/font/NewFont-Regular.ttf';
+import './assets/font/N1MONO-MONO.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
